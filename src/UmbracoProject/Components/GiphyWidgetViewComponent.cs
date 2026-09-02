0a5822aaccab6d7617548144b05b17de20d3c4a2
Christer Heinbäck <christer.heinback@consid.se>
2026-09-02 09:01:38 +0200

    Add Giphy Service

---
+++ src/UmbracoProject/Components/GiphyWidgetViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UmbracoProject.Services;
+
+namespace UmbracoProject.Components
+{
+    public class GiphyWidgetViewComponent(GiphyService svc) : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string? tag = null, string? rating = null)
+        {
+            var gif = await svc.GetRandomAsync(tag, rating);
+            return View(gif); // view handles null (renders nothing)
+        }
+    }
+
+}
